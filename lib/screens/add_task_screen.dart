3903189061cf0c64531138139dc5cb2c2831f964
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2020-02-03 13:23:54 +0700

    UI update

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -11,6 +11,22 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   String _priority;
   DateTime _date = DateTime.now();
 
+  TextEditingController _dateController = TextEditingController();
+  _handleDatePicker() async {
+    final DateTime date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _date.toString();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,14 +59,57 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
             key: _formKey,
             child: Column(
               children: <Widget>[
-                TextFormField(
-                  style: TextStyle(fontSize: 18.0),
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: TextStyle(fontSize: 18.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                // Title
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
+                    validator: (input) => input.trim().isEmpty
+                        ? 'Please enter a task title'
+                        : null,
+                    onSaved: (input) => _title = input,
+                    initialValue: _title,
+                  ),
+                ),
+                // Date Picker
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    controller: _dateController,
+                    style: TextStyle(fontSize: 18.0),
+                    onTap: _handleDatePicker,
+                    decoration: InputDecoration(
+                      labelText: 'Date',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    validator: (input) => input.trim().isEmpty
+                        ? 'Please enter a task title'
+                        : null,
+                    onSaved: (input) => _title = input,
+                    initialValue: _title,
                   ),
                 )
               ],
